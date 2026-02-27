2456b27d80362c1fa4ba7848041146f75ca67886
darrenbewsher <darren.bewsher@gmail.com>
2026-02-27 16:40:30 +0000

    Corrected listeners

--- modules/blog/main.tf
+++ modules/blog/main.tf
@@ -69,10 +69,12 @@ module "blog_alb" {
       port               = 80
       protocol           = "HTTP"
       
-      default_action = {
-        type             = "forward"
-        target_group_arn = aws_lb_target_group.blog.arn
-      }
+      default_action = [
+        {
+          type             = "forward"
+          target_group_arn = aws_lb_target_group.blog.arn
+        }
+      ]
     }
   }
 
